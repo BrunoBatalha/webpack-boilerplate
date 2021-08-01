f51fc0b70733a70a4256011f0a2574f962d01fa1
Bruno Batalha <brunobatalha37@gmail.com>
2021-08-01 17:10:14 -0400

    comment

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -3,6 +3,10 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+// imports necessários para navegadores mais antigos
+// *não consegui fazer funcionar*
+// import 'core-js';
+// import 'regenerator-runtime/runtime';
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-
+// imports necessários para navegadores mais antigos
+// *não consegui fazer funcionar*
+// import 'core-js';
+// import 'regenerator-runtime/runtime';
